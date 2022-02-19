00cc2870c6ed1e7427050fe2bbcec7d6f2142054
Srikanth Chelluri <schellluri.developer@gmail.com>
2022-02-18 20:21:42 -0500

    add skeleton for notifications on post adds

--- functions/index.js
+++ functions/index.js
@@ -1,9 +1,43 @@
 const functions = require("firebase-functions");
+const admin = require("firebase-admin");
+admin.initializeApp();
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+const db = admin.firestore();
+
+exports.addTaste = functions.firestore.document("/posts/{postId}")
+    .onCreate((snap, context) => {
+      functions.logger.log("Starting to process post", context.params.postId);
+      const data = snap.data();
+      functions.logger.log("Dumping post data", data);
+
+      const userId = data["user"]["_path"]["segments"][1];
+      const placeId = data["place"]["_path"]["segments"][1];
+
+      var friendsWhoFavorited = []
+      var friendsWhoTasted = []
+
+      return db.collection("users").doc(userId).get().then((userDoc) => {
+        functions.logger.log("userDoc", userDoc);
+        const allFriends = userDoc.data().friends;
+        functions.logger.log("all friends", allFriends);
+        allFriends.forEach()
+      });
+
+      // const favorited = data.favorited;
+
+      // if (favorited) {
+      //   for (var friend : friendsWhoFavorited) {
+      //     return
+      //   }
+      //   for (var friend : friendsWhoTasted) {
+      //     return
+      //   }
+      // } else {
+      //   for (var friend : friendsWhoFavorited) {
+      //     return
+      //   }
+      //   for (var friend : friendsWhoTasted) {
+      //     return
+      //   }
+      // }
+    });
