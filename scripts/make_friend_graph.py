bc47f7c6c986bad7641abd2c71da7726b5a378e0
Tejas Kannan <tejas.kannan286@gmail.com>
2022-02-22 20:02:35 -0600

    Add locations to users

--- scripts/make_friend_graph.py
+++ scripts/make_friend_graph.py
@@ -34,15 +34,29 @@ def parse_friend_graph(graph_path: str) -> Dict[str, List[str]]:
     return result
 
 
-def write_users(users: Dict[str, DocumentReference], friend_graph: Dict[str, List[str]]):
+def parse_user_locations(locations_path: str) -> Dict[str, str]:
+    result: Dict[str, str] = dict()
+
+    with open(locations_path, 'r') as fin:
+        reader = csv.reader(fin, delimiter=',')
+        for line in reader:
+            result[line[0]] = line[1]
+
+    return result
+
+
+def write_users(users: Dict[str, DocumentReference], friend_graph: Dict[str, List[str]], user_locations: Dict[str, str]):
     db = firestore.client()
 
     for user_handle, friend_handles in friend_graph.items():
         user_ref = users[user_handle]
         user_id = user_ref.id
 
+        location = user_locations[user_handle]
+
         user_data = {
-            'friends': [users[friend] for friend in friend_handles]
+            'friends': [users[friend] for friend in friend_handles],
+            'location': location
         }
         db.collection('users').document(user_id).set(user_data, merge=True)
 
@@ -53,6 +67,7 @@ if __name__ == '__main__':
     parser = ArgumentParser()
     parser.add_argument('--cert-path', type=str, required=True)
     parser.add_argument('--graph-path', type=str, required=True)
+    parser.add_argument('--locations-path', type=str, required=True)
     args = parser.parse_args()
 
     # Create the database client
@@ -65,8 +80,11 @@ if __name__ == '__main__':
     # Parse the friend graph
     friend_graph = parse_friend_graph(args.graph_path)
 
+    # Parse the user locations
+    user_locations = parse_user_locations(args.locations_path)
+
     # Upload the users with the friend graph
     users = load_users()
 
     # Write the updated user information to the database
-    write_users(users=users, friend_graph=friend_graph)
+    write_users(users=users, friend_graph=friend_graph, user_locations=user_locations)
