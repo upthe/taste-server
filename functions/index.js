a3ca64356a2277711b7bba5f6a82e079ba642598
Srikanth Chelluri <schellluri.developer@gmail.com>
2022-03-08 23:35:54 -0500

    add clear streaks, support local emulator

--- functions/index.js
+++ functions/index.js
@@ -158,3 +158,31 @@ exports.addNotification = functions.firestore
         });
       });
     });
+
+exports.clearStreaks = functions
+    .runWith({
+      timeoutSeconds: 300,
+    })
+    .pubsub.schedule("0 8 * * 1") // 8:00am on Mondays
+    .timeZone("America/New_York")
+    .onRun((context) => {
+      functions.logger.log("Starting to process clearing streaks");
+      const now = admin.firestore.Timestamp.now();
+      return db.collection("users").get().then((snapshot) => {
+        snapshot.docs.forEach((user) => {
+          functions.logger.log("Processing user", user.id);
+          db.collection("posts").where("user", "==", user.ref).orderBy("timestamp", "desc").limit(1).get().then((snapshot) => {
+            const latestPost = snapshot.docs[0];
+            const latestPostTimestamp = latestPost.data()["timestamp"];
+            const diffDays = (now - latestPostTimestamp) / 60 / 60 / 24;
+            functions.logger.log("Dumping latest post, user, diffDays", latestPost.id, user.id, diffDays);
+            if (diffDays > 14) {
+              functions.logger.log("Clearing streak for user", user.id);
+              user.ref.set({
+                streakCount: 0,
+              }, {merge: true});
+            }
+          });
+        });
+      });
+    });
