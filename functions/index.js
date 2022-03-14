2c5192a818c9cb86ed7fd9b57bfb18fe61994470
Srikanth Chelluri <schellluri.developer@gmail.com>
2022-03-14 09:53:54 -0400

    temp: remove award badges

--- functions/index.js
+++ functions/index.js
@@ -239,103 +239,103 @@ exports.clearStreaks = functions
 
 // Triggered every day so we can award badges
 // FUTURE: we should probably cache the cusines for each place
-exports.awardBadges = functions
-    .runWith({
-      timeoutSeconds: 300,
-    })
-    .pubsub.schedule("0 10 * * *") // 10:00am everyday
-    .timeZone("America/New_York")
-    .onRun((context) => {
-      functions.logger.log("Starting to process awarding badges");
-      const cuisineToBadgeFriendlyIdentifier = {
-        "Bagel": "Tasted10BagelPlaces",
-        "Bakery": "Tasted10BakeryPlaces",
-        "Bar": "Tasted10BarPlaces",
-        "Breakfast/Brunch": "Tasted10BreakfastBrunchPlaces",
-        "Burger": "Tasted10BurgerPlaces",
-        "Burrito/Taco": "Tasted10BurritoTacoPlaces",
-        "Cafe": "Tasted10CafePlaces",
-        "Chicken": "Tasted10ChickenPlaces",
-        "Chinese": "Tasted10ChinesePlaces",
-        "Dessert": "Tasted10DessertPlaces",
-        "Ethiopian": "Tasted10EthiopianPlaces",
-        "French": "Tasted10FrenchPlaces",
-        "Indian": "Tasted10IndianPlaces",
-        "Italian": "Tasted10ItalianPlaces",
-        "Japanese": "Tasted10JapanesePlaces",
-        "Korean": "Tasted10KoreanPlaces",
-        "Mediterranean/Middle Eastern": "Tasted10MediterraneanMiddleEasternPlaces",
-        "Mexican": "Tasted10MexicanPlaces",
-        "New American": "Tasted10NewAmericanPlaces",
-        "Pizza": "Tasted10PizzaPlaces",
-        "Ramen": "Tasted10RamenPlaces",
-        "Salad": "Tasted10SaladPlaces",
-        "Southern": "Tasted10SouthernPlaces",
-        "Sushi": "Tasted10SushiPlaces",
-        "Thai": "Tasted10ThaiPlaces",
-      };
+// exports.awardBadges = functions
+//     .runWith({
+//       timeoutSeconds: 300,
+//     })
+//     .pubsub.schedule("0 10 * * *") // 10:00am everyday
+//     .timeZone("America/New_York")
+//     .onRun((context) => {
+//       functions.logger.log("Starting to process awarding badges");
+//       const cuisineToBadgeFriendlyIdentifier = {
+//         "Bagel": "Tasted10BagelPlaces",
+//         "Bakery": "Tasted10BakeryPlaces",
+//         "Bar": "Tasted10BarPlaces",
+//         "Breakfast/Brunch": "Tasted10BreakfastBrunchPlaces",
+//         "Burger": "Tasted10BurgerPlaces",
+//         "Burrito/Taco": "Tasted10BurritoTacoPlaces",
+//         "Cafe": "Tasted10CafePlaces",
+//         "Chicken": "Tasted10ChickenPlaces",
+//         "Chinese": "Tasted10ChinesePlaces",
+//         "Dessert": "Tasted10DessertPlaces",
+//         "Ethiopian": "Tasted10EthiopianPlaces",
+//         "French": "Tasted10FrenchPlaces",
+//         "Indian": "Tasted10IndianPlaces",
+//         "Italian": "Tasted10ItalianPlaces",
+//         "Japanese": "Tasted10JapanesePlaces",
+//         "Korean": "Tasted10KoreanPlaces",
+//         "Mediterranean/Middle Eastern": "Tasted10MediterraneanMiddleEasternPlaces",
+//         "Mexican": "Tasted10MexicanPlaces",
+//         "New American": "Tasted10NewAmericanPlaces",
+//         "Pizza": "Tasted10PizzaPlaces",
+//         "Ramen": "Tasted10RamenPlaces",
+//         "Salad": "Tasted10SaladPlaces",
+//         "Southern": "Tasted10SouthernPlaces",
+//         "Sushi": "Tasted10SushiPlaces",
+//         "Thai": "Tasted10ThaiPlaces",
+//       };
 
-      return db.collection("users").get().then(async (snapshot) => {
-        snapshot.docs.forEach((user) => {
-          functions.logger.log("Processing user", user.id);
-          const userData = user.data();
-          const userBadgeFriendlyIdentifiers = userData.badgeFriendlyIdentifiers ? userData.badgeFriendlyIdentifiers : [];
-          const userTastedPlaceRefs = userData.tasted ? userData.tasted : [];
-          const userFriends = userData.friends ? userData.friends : [];
-          const userCusinesToCount = {};
+//       return db.collection("users").get().then(async (snapshot) => {
+//         snapshot.docs.forEach((user) => {
+//           functions.logger.log("Processing user", user.id);
+//           const userData = user.data();
+//           const userBadgeFriendlyIdentifiers = userData.badgeFriendlyIdentifiers ? userData.badgeFriendlyIdentifiers : [];
+//           const userTastedPlaceRefs = userData.tasted ? userData.tasted : [];
+//           const userFriends = userData.friends ? userData.friends : [];
+//           const userCusinesToCount = {};
 
-          const placeRefRequests = [];
-          userTastedPlaceRefs.forEach((placeRef) => {
-            placeRefRequests.push(placeRef.get().then((qds) => {
-              const placeData = qds.data();
-              if (placeData && "cuisines" in placeData) {
-                placeData.cuisines.forEach((cuisine) => {
-                  if (cuisine in userCusinesToCount) {
-                    userCusinesToCount[cuisine] += 1;
-                  } else {
-                    userCusinesToCount[cuisine] = 1;
-                  }
-                });
-              }
-            }));
-          });
+//           const placeRefRequests = [];
+//           userTastedPlaceRefs.forEach((placeRef) => {
+//             placeRefRequests.push(placeRef.get().then((qds) => {
+//               const placeData = qds.data();
+//               if (placeData && "cuisines" in placeData) {
+//                 placeData.cuisines.forEach((cuisine) => {
+//                   if (cuisine in userCusinesToCount) {
+//                     userCusinesToCount[cuisine] += 1;
+//                   } else {
+//                     userCusinesToCount[cuisine] = 1;
+//                   }
+//                 });
+//               }
+//             }));
+//           });
 
-          Promise.all(placeRefRequests).then(() => {
-            Object.keys(cuisineToBadgeFriendlyIdentifier).forEach(async (cuisine) => {
-              if (cuisine in userCusinesToCount && userCusinesToCount[cuisine] >= 10) {
-                const badgeFriendlyIdentifier = cuisineToBadgeFriendlyIdentifier[cuisine];
-                if (!userBadgeFriendlyIdentifiers.includes(badgeFriendlyIdentifier)) {
-                  functions.logger.log("Awarding badge to user and creating notifications, dumping badgeFriendlyIdentifier, userData.handle", badgeFriendlyIdentifier, userData.handle);
-                  await user.ref.update({
-                    badgeFriendlyIdentifiers: admin.firestore.FieldValue.arrayUnion(badgeFriendlyIdentifier),
-                  });
+//           Promise.all(placeRefRequests).then(() => {
+//             Object.keys(cuisineToBadgeFriendlyIdentifier).forEach(async (cuisine) => {
+//               if (cuisine in userCusinesToCount && userCusinesToCount[cuisine] >= 10) {
+//                 const badgeFriendlyIdentifier = cuisineToBadgeFriendlyIdentifier[cuisine];
+//                 if (!userBadgeFriendlyIdentifiers.includes(badgeFriendlyIdentifier)) {
+//                   functions.logger.log("Awarding badge to user and creating notifications, dumping badgeFriendlyIdentifier, userData.handle", badgeFriendlyIdentifier, userData.handle);
+//                   await user.ref.update({
+//                     badgeFriendlyIdentifiers: admin.firestore.FieldValue.arrayUnion(badgeFriendlyIdentifier),
+//                   });
 
-                  db.collection("notifications").add({
-                    ownerId: user.id,
-                    notificationDataBadgeFriendlyIdentifier: badgeFriendlyIdentifier,
-                    type: "BadgeAwardedToYou",
-                    seen: false,
-                    timestamp: admin.firestore.Timestamp.now(),
-                  });
+//                   db.collection("notifications").add({
+//                     ownerId: user.id,
+//                     notificationDataBadgeFriendlyIdentifier: badgeFriendlyIdentifier,
+//                     type: "BadgeAwardedToYou",
+//                     seen: false,
+//                     timestamp: admin.firestore.Timestamp.now(),
+//                   });
 
-                  userFriends.forEach((userFriend) => {
-                    const userFriendId = userFriend.id;
-                    db.collection("notifications").add({
-                      ownerId: userFriendId,
-                      notificationDataUserId: user.id,
-                      notificationDataBadgeFriendlyIdentifier: badgeFriendlyIdentifier,
-                      type: "BadgeAwardedToFriend",
-                      seen: false,
-                      timestamp: admin.firestore.Timestamp.now(),
-                    });
-                  });
-                }
-              }
-            });
-          });
-        });
-      });
-    });
+//                   userFriends.forEach((userFriend) => {
+//                     const userFriendId = userFriend.id;
+//                     db.collection("notifications").add({
+//                       ownerId: userFriendId,
+//                       notificationDataUserId: user.id,
+//                       notificationDataBadgeFriendlyIdentifier: badgeFriendlyIdentifier,
+//                       type: "BadgeAwardedToFriend",
+//                       seen: false,
+//                       timestamp: admin.firestore.Timestamp.now(),
+//                     });
+//                   });
+//                 }
+//               }
+//             });
+//           });
+//         });
+//       });
+//     });
 
 // Triggered every day to set 'cuisines' field to be an empty list as needed
 // so we can filter places for manual entry
