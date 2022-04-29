8e1698e7868a150f0ed9dbe927dee1b073bb0813
Srikanth Chelluri <schellluri.developer@gmail.com>
2022-04-29 18:04:31 -0400

    initial complete taste emerald processing

--- scripts/create_emerald_events.py
+++ scripts/create_emerald_events.py
@@ -1,52 +1,21 @@
 #!/usr/bin/env python3
 import argparse
+import csv
 import datetime
 import firebase_admin
-import hashlib
 import json
 import pytz
 from firebase_admin import credentials, firestore
 from os import environ
 
 '''
-[5 creds] Tasting a place
-[10 creds] Tasting a place that none of your friends have tasted
-    for each place
-        get list of posts in decending order
-        for each person that tasted
-            find their friend graphs
-            figure out if they're the first of their friends
-            ensure same person don't get creds multiple times for the same place
-[20 creds] Tasting a place that makes your friend want to taste it
-    for each place
-        get list of people who want to taste
-        for each person that tasted (for 3 to 5 stars)
-            find their friend graphs
-            find intersection of friend graph and all people who want to taste
-[50 creds] Tasting a place that makes your friend go taste it
-    only care if post is 3 or higher
-    for each place
-        get list of people who tasted in decending order
-        remove duplicates in list except for earliest posts
-        for each person in that list (for 3 to 5 stars)
-            get their friend graphs
-            if post is 5 star:
-                create 5 star event or each friend in list after them
-            else:
-                create event for each friend in list after them
-[100 creds] Tasting a place that makes your friend taste it and they say its excellent
-    (see above)
-'''
+Type                                Data
 
-
-'''
-Type                                Data                Uniqueness
-
-UserPostedTaste                     Post ID             (queue)
-UserTastedPlaceFirst                Post ID             (queue)
-FriendWantsToTastePlaceYouTasted    Place, user         (queue)
-FriendTastedPlaceYouTasted          Post ID (friend)    (queue)
-FriendLikedPlaceYouTasted           Post ID (friend)    (queue)
+UserPostedTaste                     Post ID
+UserTastedPlaceFirst                Post ID
+FriendWantsToTastePlaceYouTasted    Place ID, user ID (friend)
+FriendTastedPlaceYouTasted          Post ID (friend)
+FriendLikedPlaceYouTasted           Post ID (friend)
 '''
 
 def _get_post_ids_to_data(db):
@@ -88,39 +57,52 @@ def _get_place_ids_to_data(db):
     for p in places:
         place_dict = p.to_dict()
         place_ids_to_data[p.id] = {
+            'id': p.id,
             'name': place_dict.get('name')
         }
     return place_ids_to_data
 
-def _get_place_ids_to_post_ids(post_ids_to_data):
+# TODO: this must get data from the 'queueposts' collection, similar to how
+# the 'queuewanttotastes' collection works
+def _get_queue_post_ids_to_data(db, post_ids_to_data):
+    print('Getting posts queue...')
+    return post_ids_to_data
+    # queue_posts_ids = [p.id for p in db.collection('queueposts').stream()]
+    # return {
+    #     p: post_ids_to_data[p.id] for p in post_ids_to_data if p in queue_posts_ids
+    # }
+
+def _get_queue_want_to_taste_ids_to_data(db):
+    print('Getting want to taste queue...')
+    queue_want_to_taste_ids_to_data = {}
+    want_to_tastes = db.collection('queuewanttotastes').stream()
+    for w in want_to_tastes:
+        want_to_taste_dict = w.to_dict()
+        queue_want_to_taste_ids_to_data[w.id] = {
+            'id': w.id,
+            'user': w.get('user').id,
+            'place': w.get('place').id,
+            'timestamp': w.get('timestamp')
+        }
+    return queue_want_to_taste_ids_to_data
+
+def _get_place_ids_to_post_data(post_ids_to_data):
     print('Getting place IDs to post IDs...')
-    place_ids_to_post_ids = {}
+    place_ids_to_post_data = {}
     for post_id, post_data in post_ids_to_data.items():
         place_id = post_data['place']
-        if place_id in place_ids_to_post_ids:
-            place_ids_to_post_ids[place_id].append(post_id)
+        if place_id in place_ids_to_post_data:
+            place_ids_to_post_data[place_id].append(post_data)
         else:
-            place_ids_to_post_ids[place_id] = [post_id]
-    return place_ids_to_post_ids
+            place_ids_to_post_data[place_id] = [post_data]
+    return place_ids_to_post_data
 
-# def _get_place_ids_to_want_to_taste_user_ids(db, user_ids_to_data):
-#     print('Getting place IDs to want to taste user IDs...')
-#     place_ids_to_want_to_taste_user_ids = {}
-#     for user_id, user_data in user_ids_to_data.items():
-#         want_to_taste_place_ids = user_data['wantToTaste']
-#         for place_id in want_to_taste_place_ids:
-#             if place_id in place_ids_to_want_to_taste_user_ids:
-#                 place_ids_to_want_to_taste_user_ids[place_id].append(user_id)
-#             else:
-#                 place_ids_to_want_to_taste_user_ids[place_id] = [user_id]
-#     return place_ids_to_want_to_taste_user_ids
-
-def _get_place_ids_to_want_to_taste_data(db, place_ids_to_data):
+def _get_place_ids_to_want_to_taste_data(place_ids_to_data, queue_want_to_taste_ids_to_data):
     print('Getting place IDs to want to taste data...')
     place_ids_to_want_to_taste_data = {}
     for p in place_ids_to_data.keys():
         place_ref = db.collection('places').document(p)
-        want_to_tastes = db.collection('wanttotastes').where('place', '==', place_ref).get()
+        want_to_tastes = [w for w in queue_want_to_taste_ids_to_data if w['place'] == place_ref.id]
         want_to_taste_data = []
         for w in want_to_tastes:
             want_to_taste_data.append({
@@ -132,102 +114,158 @@ def _get_place_ids_to_want_to_taste_data(db, place_ids_to_data):
         place_ids_to_want_to_taste_data[p] = want_to_taste_data
     return place_ids_to_want_to_taste_data
 
-# todo
-def _get_current_events(db):
-    return {}
-
-def _get_hash_string(*strs):
-    to_hash = [s for s in strs]
-    return hashlib.md5(''.join(to_hash).encode()).hexdigest()
-
-# Create 'UserPostedTaste' events
-def create_events_for_user_posted_taste(events, post_ids_to_data):
-    for post_id, post_data in post_ids_to_data.items():
-        key = _get_hash_string(post_id)
-        if key in events:
-            continue
-        timestamp = datetime.datetime.now()
-        events[key] = {
+# create 'UserPostedTaste' events whenever a user posts a taste
+def create_events_for_user_posted_taste(queue_post_ids_to_data):
+    events = []
+    for post_id, post_data in queue_post_ids_to_data.items():
+        events.append({
             'type': 'UserPostedTaste',
             'user': post_data['user'],
             'data': {
                 'post': post_id
             },
-            'timestamp': timestamp
-        }
+            'timestamp': datetime.datetime.now()
+        })
     return events
 
-# Create 'UserTastedPlaceFirst' events
-def create_events_for_user_tasted_place_first(events, place_ids_to_post_ids, post_ids_to_data, user_ids_to_data):
-    for place_id, post_ids in place_ids_to_post_ids.items():
-        # todo: filter based on post retastes, not field
-        posts = [post_ids_to_data[p] for p in post_ids if not post_ids_to_data[p]['retaste']]
-        posts = sorted(posts, key=lambda p: p['timestamp'], reverse=True)
+# create 'UserTastedPlaceFirst' events whenever a user posts a taste that is
+# the first taste of that place within their friend graph
+def create_events_for_user_tasted_place_first(place_ids_to_post_data, post_ids_to_data, user_ids_to_data):
+    events = []
+    for place_id, place_post_data in place_ids_to_post_data.items():
+        # get sorted list of posts filtering out retastes
+        sorted_place_post_data = sorted(place_post_data, key=lambda p: p['timestamp'])
+        sanitized_place_post_data = []
+        for p in sorted_place_post_data:
+            if p['user'] in [p['user'] for p in sanitized_place_post_data]:
+                continue
+            sanitized_place_post_data.append(p)
 
-        users = [p['user'] for p in posts]
-        for u in users:
-            user_friends = user_ids_to_data[u]['friends']
-            posts_filtered = [p for p in posts if p['user'] in user_friends or p['user'] == u]
-            first_post = posts_filtered[-1]
+        place_users = [p['user'] for p in sanitized_place_post_data]
+        for place_user in place_users:
+            user_friend_ids = user_ids_to_data[place_user]['friends']
+            place_posts_filtered = [p for p in sanitized_place_post_data if p['user'] in user_friend_ids or p['user'] == place_user]
+            place_first_post_for_friends = place_posts_filtered[0]
             # prevent creating event for same person in multiple friend graphs
-            if first_post['user'] == u:
-                key = _get_hash_string(first_post['id'])
-                if key in events:
-                    continue
-                timestamp = datetime.datetime.now()
-                events[key] = {
-                    'type': 'UserTastedPlaceFirst',
-                    'user': u,
-                    'data': {
-                        'post': first_post['id']
-                    },
-                    'timestamp': timestamp
-                }
+            if place_first_post_for_friends['user'] != place_user:
+                continue
+            events.append({
+                'type': 'UserTastedPlaceFirst',
+                'user': place_user,
+                'data': {
+                    'post': place_first_post_for_friends['id']
+                },
+                'timestamp': datetime.datetime.now()
+            })
     return events
 
-# Create 'FriendWantsToTastePlaceYouTasted' events
-def create_events_for_friend_wants_to_taste_place_you_tasted(events, place_ids_to_post_ids, place_ids_to_want_to_taste_data, post_ids_to_data, user_ids_to_data):
-    place_ids = set(place_ids_to_post_ids.keys()) | set(place_ids_to_want_to_taste_data.keys())
-    for p in place_ids:
-        want_to_taste_data = place_ids_to_want_to_taste_data.get(p, [])
-        post_ids = place_ids_to_post_ids.get(p, [])
-        # todo: filter based on post retastes, not field
-        posts = [post_ids_to_data[p] for p in post_ids if not post_ids_to_data[p]['retaste']]
+# create 'FriendWantsToTastePlaceYouTasted' events whenever a user's friend
+# wants to taste a place that the user tasted if both the user's friend's
+# post and the user's post is at least 3 stars
+def create_events_for_friend_wants_to_taste_place_you_tasted(place_ids_to_post_data, place_ids_to_want_to_taste_data, post_ids_to_data, user_ids_to_data):
+    events = []
+    place_ids = set(place_ids_to_post_data.keys()) | set(place_ids_to_want_to_taste_data.keys())
+    for place_id in place_ids:
+        place_post_data = place_ids_to_post_data.get(place_id, [])
+        place_want_to_taste_data = place_ids_to_want_to_taste_data.get(place_id, [])
 
-        for d in want_to_taste_data:
-            user_id = d['user']
-            timestamp = d['timestamp']
-            user_friends = user_ids_to_data[user_id]['friends']
-            posts_filtered = [p for p in posts if p['user'] in user_friends and p['starRating'] >= 3 and p['timestamp'] < timestamp]
-            # print(posts_filtered)
-            # print()
-            print('processing user', user_id, d['id'])
-            for pf in posts_filtered:
-                print(pf)
-                key = _get_hash_string(d['id'])
-                if key in events:
-                    continue
-                timestamp = datetime.datetime.now()
-                events[key] = {
+        # get sorted list of posts filtering out star ratings less than 3 and retastes
+        sorted_place_post_data = sorted(place_post_data, key=lambda p: p['timestamp'])
+        filtered_place_post_data = [p for p in sorted_place_post_data if p['starRating'] >= 3]
+        sanitized_place_post_data = []
+        for p in filtered_place_post_data:
+            if p['user'] in [p['user'] for p in sanitized_place_post_data]:
+                continue
+            sanitized_place_post_data.append(p)
+
+        for place_want_to_taste in place_want_to_taste_data:
+            user_id = place_want_to_taste['user']
+            user_friend_ids = user_ids_to_data[user_id]['friends']
+            place_posts_filtered = [p for p in sanitized_place_post_data if p['user'] in user_friend_ids and p['starRating'] >= 3]
+            for place_post in place_posts_filtered:
+                events.append({
                     'type': 'FriendWantsToTastePlaceYouTasted',
-                    'user': pf['user'],
+                    'user': place_post['user'],
                     'data': {
-                        'place': p,
+                        'place': place_id,
                         'user': user_id
                     },
-                    'timestamp': timestamp
+                    'timestamp': datetime.datetime.now()
+                })
+    return events
+
+
+# create 'FriendTastedPlaceYouTasted' events whenever a user's friend tastes
+# a place that the user tasted if the user's taste is at least 3 stars and the
+# friend's taste is either 3 or 4 stars; create 'FriendLikedPlaceYouTasted'
+# whenever a user's friend tastes a place that the user tasted if the user's
+# taste is at least 3 stars and the friend's taste is 5 stars
+def create_events_for_friend_tasted_liked_place_you_tasted(place_ids_to_post_data, post_ids_to_data, user_ids_to_data):
+    events = []
+    for place_id, place_post_data in place_ids_to_post_data.items():
+        # get sorted list of posts filtering out star ratings less than 3 and retastes
+        sorted_place_post_data = sorted(place_post_data, key=lambda p: p['timestamp'])
+        filtered_place_post_data = [p for p in sorted_place_post_data if p['starRating'] >= 3]
+        sanitized_place_post_data = []
+        for p in filtered_place_post_data:
+            if p['user'] in [p['user'] for p in sanitized_place_post_data]:
+                continue
+            sanitized_place_post_data.append(p)
+
+        place_users_to_ranks = [(p['id'], p['user'], p['starRating']) for p in sanitized_place_post_data]
+        for i in range(len(place_users_to_ranks) - 1):
+            curr_post_id = place_users_to_ranks[i][0]
+            curr_user_id = place_users_to_ranks[i][1]
+            curr_user_friends = user_ids_to_data[curr_user_id]['friends']
+            for j in range(i + 1, len(place_users_to_ranks)):
+                comp_post_id = place_users_to_ranks[j][0]
+                comp_user_id = place_users_to_ranks[j][1]
+                comp_star_rating = place_users_to_ranks[j][2]
+                if comp_user_id not in curr_user_friends:
+                    continue
+                payload = {
+                    'user': curr_user_id,
+                    'data': {
+                        'post': comp_user_id
+                    },
+                    'timestamp': datetime.datetime.now()
                 }
-            print()
+                if comp_star_rating == 5:
+                    events.append({**{'type': 'FriendLikedPlaceYouTasted'}, **payload})
+                else:
+                    events.append({**{'type': 'FriendTastedPlaceYouTasted'}, **payload})
     return events
 
-def create_events_for_friend_tasted_liked_place_you_tasted(place_ids_to_post_ids, post_ids_to_data, user_ids_to_data):
-    events = {}
-    for place_ids, post_ids in place_ids_to_post_ids.items():
-        posts = [post_ids_to_data[p] for p in post_ids if not post_ids_to_data[p]['retaste'] and post_ids_to_data[p]['starRating'] >= 3]
-        posts = sorted(posts, key=lambda p: p['timestamp'], reverse=True)
+def output_events(events):
+    fields = ['user', 'type', 'timestamp', 'post', 'place', 'user']
+    rows = []
+    for e in events:
+        event_type = e['type']
+        data = e['data']
+        row = [e['user'], event_type, e['timestamp']]
+        if event_type == 'UserPostedTaste':
+            row.extend([data['post'], None, None])
+        elif event_type == 'UserTastedPlaceFirst':
+            row.extend([data['post'], None, None])
+        elif event_type == 'FriendWantsToTastePlaceYouTasted':
+            row.extend([None, data['place'], data['user']])
+        elif event_type == 'FriendTastedPlaceYouTasted':
+            row.extend([data['post'], None, None])
+        elif event_type == 'FriendLikedPlaceYouTasted':
+            row.extend([data['post'], None, None])
+        rows.append(row)
+    with open(f'tmp/events.csv', 'w') as f:
+        writer = csv.writer(f)
+        writer.writerow(fields)
+        writer.writerows(rows)
 
+# TODO
+def publish_events(db, events):
+    pass
 
-    return events
+# TODO
+def clear_queues(db):
+    pass
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
@@ -248,41 +286,26 @@ if __name__ == '__main__':
 
     db = firestore.client()
 
+    # cache raw data
     user_ids_to_data = _get_user_ids_to_data(db)
     post_ids_to_data = _get_post_ids_to_data(db)
     place_ids_to_data = _get_place_ids_to_data(db)
 
-    queue_post_ids = _get_queue_post_ids_to_data(db)
-    queue_want_to_taste_ids = _get_queue_want_to_taste_ids(db)
+    # get documents to process
+    queue_post_ids_to_data = _get_queue_post_ids_to_data(db, post_ids_to_data)
+    queue_want_to_taste_ids_to_data = _get_queue_want_to_taste_ids_to_data(db)
 
-    place_ids_to_post_ids = _get_place_ids_to_post_ids(post_ids_to_data)
-    place_ids_to_want_to_taste_data = _get_place_ids_to_want_to_taste_data(db, place_ids_to_data)
+    # create maps based on places
+    place_ids_to_post_data = _get_place_ids_to_post_data(post_ids_to_data)
+    place_ids_to_want_to_taste_data = _get_place_ids_to_want_to_taste_data(place_ids_to_data, queue_want_to_taste_ids_to_data)
 
-    events = _get_current_events(db)
+    # create events
+    events = []
+    events.extend(create_events_for_user_posted_taste(queue_post_ids_to_data))
+    events.extend(create_events_for_user_tasted_place_first(place_ids_to_post_data, post_ids_to_data, user_ids_to_data))
+    events.extend(create_events_for_friend_wants_to_taste_place_you_tasted(place_ids_to_post_data, place_ids_to_want_to_taste_data, post_ids_to_data, user_ids_to_data))
+    events.extend(create_events_for_friend_tasted_liked_place_you_tasted(place_ids_to_post_data, post_ids_to_data, user_ids_to_data))
 
-    # events = {**events, **create_events_for_user_posted_taste(events, post_ids_to_data)}
-
-    # events = {**events, **create_events_for_user_tasted_place_first(events, place_ids_to_post_ids, post_ids_to_data, user_ids_to_data)}
-    # for e, d in events.items():
-    #     handle = user_ids_to_data[d['user']]['handle']
-    #     post_id = d['data']
-    #     place_id = post_ids_to_data[post_id]['place']
-    #     place_name = place_ids_to_data[place_id]['name']
-    #     # name = place_ids_to_data[post_ids_to_data[d['data']]]['name']
-    #     print(handle, place_name)
-
-    events = {**events, **create_events_for_friend_wants_to_taste_place_you_tasted(events, place_ids_to_post_ids, place_ids_to_want_to_taste_data, post_ids_to_data, user_ids_to_data)}
-    # prints = []
-    # for e, d in events.items():
-    #     user_id = d['user']
-    #     place_id = d['data']['place']
-    #     friend_id = d['data']['user']
-    #     prints.append(f"{place_ids_to_data[place_id]['name']} - {user_ids_to_data[friend_id]['handle']} wants to taste, crediting {user_ids_to_data[user_id]['handle']}")
-    # prints = sorted(prints)
-    # for p in prints:
-    #     print(p)
-
-        
-    # for e, d in events.items():
-    #     print(d)
-    # print(len(events))
+    output_events(events)
+    publish_events(db, events)
+    clear_queues(db)
