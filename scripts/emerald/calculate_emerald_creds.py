cbb0334ec20ef960e571823d733b3678fac8d53f
Srikanth Chelluri <schellluri.developer@gmail.com>
2022-05-02 23:09:18 -0400

    award and remove emerald status, no db update and no notifs right now

--- scripts/emerald/calculate_emerald_creds.py
+++ scripts/emerald/calculate_emerald_creds.py
@@ -18,7 +18,8 @@ def _get_user_ids_to_data(db):
             'firstName': user_dict.get('firstName'),
             'lastName': user_dict.get('lastName'),
             'handle': user_dict.get('handle'),
-            'wantToTaste': [p.id for p in user_dict.get('wantToTaste')],
+            'emerald': user_dict.get('emerald', False),
+            'emeraldCreds': user_dict.get('emeraldCreds', 0),
             'friends': [f.id for f in user_dict.get('friends')]
         }
     return user_ids_to_data
@@ -40,6 +41,7 @@ def set_user_creds(db, user_ids_to_data, event_ids_to_data):
         'FriendTastedPlaceYouTasted': 4,
         'FriendLikedPlaceYouTasted': 5
     }
+    user_ids_to_creds = {}
 
     for u in user_ids_to_data:
         print(f'Processing user {u}...')
@@ -48,9 +50,46 @@ def set_user_creds(db, user_ids_to_data, event_ids_to_data):
         for event in events:
             event_type = event['type']
             creds += event_type_to_creds[event_type]
+        user_ids_to_creds[u] = creds
         db.collection('users').document(u).update({
             'emeraldCreds': creds
         })
+    return user_ids_to_creds
+
+def calculate_emerald_user_ids(user_ids_to_creds):
+    tuples = [(u, c) for u, c in user_ids_to_creds.items()]
+    tuples = sorted(tuples, key=lambda t: t[1], reverse=True)
+
+    emerald_user_ids = []
+    num_emerald_ranks = int(len(user_ids_to_creds) * 0.1)
+    i = 0
+    while i < num_emerald_ranks:
+        user_id, creds = tuples[i]
+        emerald_user_ids.append(user_id)
+        j = i + 1
+        while j < len(user_ids_to_creds):
+            next_user_id, next_creds = tuples[j]
+            if next_creds == creds:
+                emerald_user_ids.append(next_user_id)
+                j += 1
+            else:
+                break
+        i = j
+    return emerald_user_ids
+
+def process_emerald_statuses(db, user_ids_to_data, current_emerald_user_ids, updated_emerald_user_ids):
+    remove_emerald_user_ids = list(set(current_emerald_user_ids) - set(updated_emerald_user_ids))
+    award_emerald_user_ids = list(set(updated_emerald_user_ids) - set(current_emerald_user_ids))
+    for u in remove_emerald_user_ids:
+        print(f'Removing Emerald status from {user_ids_to_data[u]["handle"]}...')
+        # db.collection('users').document(u).update({
+        #     'emerald': False
+        # })
+    for u in award_emerald_user_ids:
+        print(f'Awarding Emerald status to {user_ids_to_data[u]["handle"]}...')
+        # db.collection('users').document(u).update({
+        #     'emerald': True
+        # })
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
@@ -73,4 +112,7 @@ if __name__ == '__main__':
 
     user_ids_to_data = _get_user_ids_to_data(db)
     event_ids_to_data = _get_event_ids_to_data(db)
-    set_user_creds(db, user_ids_to_data, event_ids_to_data)
+    user_ids_to_creds = set_user_creds(db, user_ids_to_data, event_ids_to_data)
+    current_emerald_user_ids = [u for u, d in user_ids_to_data.items() if d['emerald']]
+    updated_emerald_user_ids = calculate_emerald_user_ids(user_ids_to_creds)
+    process_emerald_statuses(db, user_ids_to_data, current_emerald_user_ids, updated_emerald_user_ids)
