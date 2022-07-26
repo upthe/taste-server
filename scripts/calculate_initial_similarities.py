df572b422164094202c70c02a0120f1ff112488d
Srikanth Chelluri <schellluri.developer@gmail.com>
2022-07-25 23:04:33 -0400

    remove check

--- scripts/calculate_initial_similarities.py
+++ scripts/calculate_initial_similarities.py
@@ -61,9 +61,6 @@ def upsert_similarity(db, friend_set, user_ids_to_data):
     for place_id in common_places:
         u1_posts = [p for p, d in post_ids_to_data.items() if d['place'] == place_id and d['user'] == u1]
         u2_posts = [p for p, d in post_ids_to_data.items() if d['place'] == place_id and d['user'] == u2]
-        if len(u1_posts) == 0 or len(u2_posts) == 0:
-            continue
-
         u1_star_ratings = [post_ids_to_data[p]['starRating'] for p in u1_posts]
         u2_star_ratings = [post_ids_to_data[p]['starRating'] for p in u2_posts]
         u1_avg_star_rating = sum(u1_star_ratings) / len(u1_star_ratings)
